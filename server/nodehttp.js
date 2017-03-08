5cc8c6ff980c1c2744d0f0276757a1a32db4c1b4
alex <alexbonavila44@gmail.com>
2017-03-08 19:32:03 +0100

    node http example

---
+++ server/nodehttp.js
@@ -0,0 +1,13 @@
+
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(client){
+    console.log("Connected");
+  client.on('event', function(data){
+
+  });
+  client.on('disconnect', function(){
+
+  });
+});
+server.listen(3001);
