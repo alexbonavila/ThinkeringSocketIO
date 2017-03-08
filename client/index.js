ba36ee3a4e65fe0ea3d4ee4a7ec038722d96546c
alex <alexbonavila44@gmail.com>
2017-03-08 19:45:33 +0100

    Client Example

---
+++ client/index.js
@@ -0,0 +1,10 @@
+var socket = require('socket.io-client')('http://localhost');
+socket.on('connect', function(){
+
+});
+socket.on('event', function(data){
+
+});
+socket.on('disconnect', function(){
+
+});
