3c1c344462f632cef9d7a2df9c0ac445380a0f13
alex <alexbonavila44@gmail.com>
2017-03-08 19:56:01 +0100

    Enviem un esdeveniment al client

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function(){
     console.log('Client connected ok to localhost port 3000')
 });
-socket.on('event', function(data){
+socket.on('Hello', function(data){
     console.log('Event Hello received')
 });
 socket.on('disconnect', function(){
--- server/standalone.js
+++ server/standalone.js
@@ -1,6 +1,8 @@
 var io = require('socket.io')();
 io.on('connection', function(client){
     console.log('Client connected');
-    console.log(client);
+    //console.log(client);
+    client.emit('Hello','test hello event')
+    client.disconnect()
 });
 io.listen(3000);
